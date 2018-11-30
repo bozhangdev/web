e44ffc468c5a2ae98962713de9ad3b75c9bb4d4a
bo.zhang <bo.zhang@etu.unice.fr>
2018-11-30 09:39:50 +0800

    add nginx

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 source env/bin/activate
 pip install -r requirements.txt
-env/bin/gunicorn -b 0.0.0.0:8080 wsgi:app >& out.log &
+env/bin/gunicorn -w 4 -b 0.0.0.0:8080 wsgi:app >& out.log &
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
 from app import app
 if __name__ == 'main':
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run()         
